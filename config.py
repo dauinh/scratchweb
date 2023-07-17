0f6a335a967597a53ccb99e9ddb4c30e431947dd
linh <linh.ha.nguyen20@gmail.com>
2023-07-17 15:33:10 +0700

    refactor templates to display profile

--- config.py
+++ config.py
@@ -1,5 +1,19 @@
 import os
 
+# Class-based application configuration
+# class ConfigClass(object):
+#     """ Flask application config """
+
+# Flask-SQLAlchemy settings
+SQLALCHEMY_DATABASE_URI = 'sqlite:///scratchweb.sqlite'    # File-based SQL database
+SQLALCHEMY_TRACK_MODIFICATIONS = False    # Avoids SQLAlchemy warning
+
+# Flask-User settings
+USER_APP_NAME = "Scratch App"     # Shown in and email templates and page footers
+USER_ENABLE_EMAIL = False      # Disable email authentication
+USER_ENABLE_USERNAME = True    # Enable username authentication
+USER_REQUIRE_RETYPE_PASSWORD = False    # Simplify register form
+
 OIDC_CLIENT_ID = os.getenv('OIDC_CLIENT_ID')
 OIDC_CLIENT_SECRET = os.getenv('OIDC_CLIENT_SECRET')
 APP_SECRET = os.getenv('APP_SECRET')
--- oidc.py
+++ oidc.py
@@ -4,58 +4,113 @@ from flask import Flask, url_for, session
 from flask import render_template, redirect
 from authlib.integrations.flask_client import OAuth
 
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_user import login_required, UserManager, UserMixin
 
 CONF_URL = 'https://pressingly-account.onrender.com'
 
-app = Flask(__name__)
-app.config.from_object('config')
-app.secret_key = app.config['APP_SECRET']
 
-oauth = OAuth(app)
-oauth.register(
-    name='pressingly',
-    client_id=app.config['OIDC_CLIENT_ID'],
-    client_secret=app.config['OIDC_CLIENT_SECRET'],
-    access_token_url=f'{CONF_URL}/oauth/token',
-    access_token_params=None,
-    authorize_url=f'{CONF_URL}/oauth/authorize',
-    authorize_params=None,
-    # api_base_url='https://api.github.com/',
-    client_kwargs={'scope': 'openid email'},
-    server_metadata_url=f'{CONF_URL}/.well-known/openid-configuration'
-)
+# Initialize Flask extensions
+db = SQLAlchemy()
+oauth = OAuth()
+
+def create_app():
+    """ Flask application factory """
+    
+    # Create Flask app load app.config
+    app = Flask(__name__)
+    app.config.from_object('config')
+    app.secret_key = app.config['APP_SECRET']
+
+    # Setup Flask-SQLAlchemy
+    db.init_app(app)
+
+    oauth.init_app(app)
+    oauth.register(
+        name='pressingly',
+        client_id=app.config['OIDC_CLIENT_ID'],
+        client_secret=app.config['OIDC_CLIENT_SECRET'],
+        access_token_url=f'{CONF_URL}/oauth/token',
+        access_token_params=None,
+        authorize_url=f'{CONF_URL}/oauth/authorize',
+        authorize_params=None,
+        # api_base_url='https://api.github.com/',
+        client_kwargs={'scope': 'openid email'},
+        server_metadata_url=f'{CONF_URL}/.well-known/openid-configuration'
+    )
+
+    pressingly = oauth.pressingly
+
+    class User(db.Model, UserMixin):
+        __tablename__ = 'users'
+        id = db.Column(db.Integer, primary_key=True)
+        active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
+
+        # User authentication information. The collation='NOCASE' is required
+        # to search case insensitively when USER_IFIND_MODE is 'nocase_collation'.
+        username = db.Column(db.String(100, collation='NOCASE'), nullable=False, unique=True)
+        password = db.Column(db.String(255), nullable=False, server_default='')
+        email_confirmed_at = db.Column(db.DateTime())
+
+        # User information
+        first_name = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
+        last_name = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
 
 
-pressingly = oauth.pressingly
+    with app.app_context():
+        db.create_all()
 
-@app.route('/')
-def homepage():
-    return render_template('home.html')
-
-
-@app.route('/login')
-def login():
-    pressingly = oauth.create_client('pressingly')
-    redirect_uri = url_for('auth', _external=True)
-    return pressingly.authorize_redirect(redirect_uri)
+        # from models.user_model import User
+        user_manager = UserManager(app, db, User)
 
 
-@app.route('/auth')
-def auth():
-    token = oauth.pressingly.authorize_access_token()
-    if token:
-        session['token'] = token
-    return redirect('/profile')
+        @app.context_processor
+        def context_processor():
+            return dict(user_manager=user_manager)
 
 
-@app.route('/profile')
-def profile():
-    token = session['token']
-    access_token = token['access_token']
-    userInfoEndpoint = f'{CONF_URL}/oauth/userinfo'
-    userInfoResponse = requests.post(userInfoEndpoint,
-                                    headers={'Authorization': f'Bearer {access_token}', 'Accept': 'application/json'})
-    return render_template('profile.html', data=userInfoResponse.json())
+        @app.route('/')
+        def home():
+            return render_template('home.html')
 
+
+        @app.route('/login')
+        def login():
+            pressingly = oauth.create_client('pressingly')
+            redirect_uri = url_for('auth', _external=True)
+            return pressingly.authorize_redirect(redirect_uri)
+
+
+        @app.route('/auth')
+        def auth():
+            token = oauth.pressingly.authorize_access_token()
+            if token:
+                session['token'] = token
+            return redirect('/profile')
+
+
+        @app.route('/logout')
+        def logout():
+            session['token'] = None
+            return redirect('/')
+
+
+        @app.route('/profile')
+        @login_required
+        def profile():
+            token = session['token']
+            if not token:
+                return redirect('/login')
+            access_token = token['access_token']
+            userInfoEndpoint = f'{CONF_URL}/oauth/userinfo'
+            userInfoResponse = requests.post(userInfoEndpoint,
+                                            headers={'Authorization': f'Bearer {access_token}', 'Accept': 'application/json'})
+            return render_template('profile.html', data=userInfoResponse.json())
+    return app
+
+
+# Start development web server
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', debug=True, ssl_context="adhoc")
+    app = create_app()
+    app.run(host='127.0.0.1', port=5000, debug=True, ssl_context="adhoc")
